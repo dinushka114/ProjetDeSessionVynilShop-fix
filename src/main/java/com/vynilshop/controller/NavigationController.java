f54408b3b636f21f7eba6ca587fe1bfc804cc319
Dinesh_Saman <it20653286@my.sliit.lk>
2023-09-26 09:55:05 +0530

    Application Error Disclosure Fixed

--- src/main/java/com/vynilshop/controller/NavigationController.java
+++ src/main/java/com/vynilshop/controller/NavigationController.java
@@ -5,7 +5,6 @@
 package com.vynilshop.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -27,41 +26,46 @@ public class NavigationController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-       protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String action = request.getParameter("to");
-        
-        response.setHeader("Content-Security-Policy", "default-src 'self'");
-        
+
         try {
             if (action.equals("adminProducts")) {
-                // Forward to the products page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/products.jsp");
+                requestDispatcher.forward(request, response);
             } else if (action.equals("adminAddNewProduct")) {
-                // Forward to the add-product page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/add-product.jsp");
+                requestDispatcher.forward(request, response);
             } else if (action.equals("adminUpdateProduct")) {
-                // Forward to the update-product page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/add-product.jsp");
+                requestDispatcher.forward(request, response);
             } else if (action.equals("adminUsers")) {
-                // Forward to the customers page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/customers.jsp");
+                requestDispatcher.forward(request, response);
             } else if (action.equals("adminDashboard")) {
-                // Forward to the admin-dashboard page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/admin-dashboard.jsp");
+                requestDispatcher.forward(request, response);
             } else if (action.equals("adminOrders")) {
-                // Forward to the orders page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/orders.jsp");
+                requestDispatcher.forward(request, response);
             } else if (action.equals("myOrders")) {
-                // Forward to the my-orders page
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/my-orders.jsp");
+                requestDispatcher.forward(request, response);
             } else {
                 // Handle unexpected action value
                 throw new ServletException("Invalid action: " + action);
             }
-        } catch (Exception e) {
+        } catch (IOException | ServletException e) {
             // Set the error message as an attribute
             request.setAttribute("errorMessage", "An error occurred: " + e.getMessage());
-            
+
             // Forward to the custom error page
             request.getRequestDispatcher("/error.jsp").forward(request, response);
-            return; // Ensure that no further processing occurs after forwarding
+
         }
-        
+
         // Continue with normal processing
     }
 
